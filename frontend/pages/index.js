37f84cd74e86f316e096d875d47314a8f0886e87
Taylor Jones <taylorjonesdev@gmail.com>
2019-02-19 14:54:20 -0600

    Started testing w/ implementing Material-UI

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import fetch from 'isomorphic-unfetch';
+import Button from '@material-ui/core/Button';
+import { Container, Row, Col } from 'react-grid-system';
+import { ShowAt } from 'react-with-breakpoints';
 import Link from 'next/link';
+import { FaChevronRight } from 'react-icons/fa';
 import PropTypes from 'prop-types';
-import Layout from '../components/UI/Layout/Layout';
 import withPageWrapper from '../hoc/withPageWrapper';
+import Layout from '../components/UI/Layout/Layout';
 import HeroImage from '../components/UI/HeroImage/HeroImage';
 import { Config } from '../config';
+import css from './index.scss';
 
 
 class Index extends Component {
@@ -15,21 +20,59 @@ class Index extends Component {
     return { page };
   }
 
+
+  state = {
+    emailAddress: '',
+    subscribeEnabled: false,
+  }
+
+
+  /**
+   * Handles input changes to the email address input control.
+   */
+  handleEmailChange = event => {
+    const hasInput = event.target.value.length > 0;
+    this.setState({
+      emailAddress: event.target.value,
+      subscribeEnabled: hasInput,
+    });
+  }
+
+
+  /**
+   * Handles a submission of email address for subscription.
+   */
+  handleEmailAddressSubmission = event => {
+    event.preventDefault();
+    console.log(this.state);
+    // TODO: Handle the backend stuff. Maybe a custom post type of Subscribers needs to be created?
+  }
+
+
+
   render() {
+    // console.log(this.props);
+    const { page, headerMenu, drawerMenu, footerMenu, baseMenu } = this.props;
+
     return (
       <Layout
-        headerMenu={this.props.headerMenu}
-        drawerMenu={this.props.drawerMenu}
-        footerMenu={this.props.footerMenu}
-        baseMenu={this.props.baseMenu}
-        title={this.props.page.title}
+        headerMenu={headerMenu}
+        drawerMenu={drawerMenu}
+        footerMenu={footerMenu}
+        baseMenu={baseMenu}
+        title={page.title}
       >
         <HeroImage
-          featuredImage={this.props.page.featured_image}
-          title={this.props.page.title.rendered}
+          featuredImage={page.featured_image}
+          title={page.title.rendered}
           dotted
           absolute
         />
+
+        <Container className={css.Container}>
+          <Button>Unstyled Button</Button>
+          <Button className={css.Button}>My Button</Button>
+        </Container>
 
         <h1>{this.props.page.title.rendered}</h1>
         <div dangerouslySetInnerHTML={{ __html: this.props.page.content.rendered }} />
@@ -40,16 +83,12 @@ class Index extends Component {
 }
 
 
-Index.defaultProps = {
-  page: null,
-};
-
 Index.propTypes = {
   headerMenu: PropTypes.instanceOf(Object).isRequired,
   drawerMenu: PropTypes.instanceOf(Object).isRequired,
   footerMenu: PropTypes.instanceOf(Object).isRequired,
   baseMenu: PropTypes.instanceOf(Object).isRequired,
-  page: PropTypes.instanceOf(Object),
+  page: PropTypes.instanceOf(Object).isRequired,
 };
 
 export default withPageWrapper(Index);
